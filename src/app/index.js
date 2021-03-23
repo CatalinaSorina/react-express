fc66ae2ece2c3784b4a15281bcb2d1dd828c5d26
CatalinaSorina <gherzan.catalina@yahoo.com>
2021-03-23 16:56:56 +0200

    added store and main components

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,7 @@
 import { store } from './store';
 
-console.log('Hello react with Daniel Stern !');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Main } from './components/Main';
 
-console.log(store.getState());
+ReactDOM.render(<Main />, document.getElementById('app'));
