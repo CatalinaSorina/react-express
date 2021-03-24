31041ba2aa3e8711f3add84d40b1b98ff6286d69
CatalinaSorina <gherzan.catalina@yahoo.com>
2021-03-24 17:30:33 +0200

    added route guard with login redirect

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router, Route } from 'react-router';
+import { Router, Route, Redirect } from 'react-router';
 import { store } from '../store';
 import { history } from '../store/history';
 import { ConnectedDashboard } from './Dashboard';
+import { ConnectedLogin } from './Login';
 import { ConnectedNavigation } from './Navigation';
 import { ConnectedTaskDetail } from './TaskDetail';
 
+const RouteGuard = Component => ({ match }) => {
+  console.log('Route guard', match);
+  if (!store.getState().session.authenticated) {
+    // reroute
+    return <Redirect to='/' />;
+  }
+  return <Component match={match} />;
+};
 export const Main = () => (
   <Router history={history}>
     <Provider store={store}>
       <ConnectedNavigation />
-      <Route exact path='/dashboard' render={() => <ConnectedDashboard />} />
-      <Route
-        exact
-        path='/task/:id'
-        render={({ match }) => <ConnectedTaskDetail match={match} />}
-      />
+      <Route exact path='/' component={ConnectedLogin} />
+      <Route exact path='/dashboard' render={RouteGuard(ConnectedDashboard)} />
+      <Route exact path='/task/:id' render={RouteGuard(ConnectedTaskDetail)} />
     </Provider>
   </Router>
 );
