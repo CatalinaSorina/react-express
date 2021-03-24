e595099203ba4ef2ce7642875cd046d9c5c219a5
CatalinaSorina <gherzan.catalina@yahoo.com>
2021-03-24 19:39:21 +0200

    connecting authentication from server to client

--- src/app/store/sagas.js
+++ src/app/store/sagas.js
@@ -2,6 +2,7 @@ import { take, put, select, all } from 'redux-saga/effects';
 import uuid from 'uuid';
 import axios from 'axios';
 import * as mutations from './mutations';
+import { history } from './history';
 
 const url = 'http://localhost:4040';
 
@@ -57,6 +58,11 @@ export function* userAuthenticateSaga() {
         throw new Error();
       }
       console.log('Authenticated', data);
+
+      yield put(mutations.setState(data.state));
+      yield put(mutations.processAuthenticateUser(mutations.AUTHENTICATED));
+
+      history.push('/dashboard');
     } catch (e) {
       console.log("can't authenticate");
       yield put(mutations.processAuthenticateUser(mutations.NOT_AUTHENTICATED));
