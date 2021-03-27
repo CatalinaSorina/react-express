85f770c168fd544da638b71e18b543741559598e
CatalinaSorina <gherzan.catalina@yahoo.com>
2021-03-27 15:01:18 +0200

    move functions for netlify

--- lambda_functions/what_is_the_time.js
+++ lambda_functions/what_is_the_time.js
@@ -1,5 +1,3 @@
-// ./lambda_functions/what_is_the_time.js
-
 // This `handler` is what is called when your Lambda
 // function is triggered. For more full specs on it see
 // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html
