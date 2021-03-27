9769c8f5c8eadc8b5f1276d364f8dc198063cef3
CatalinaSorina <gherzan.catalina@yahoo.com>
2021-03-27 18:10:40 +0200

    added custom env

--- src/server/connect-db.js
+++ src/server/connect-db.js
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb';
-const url = 'mongodb+srv://React:Express@cluster0.mlkg7.mongodb.net/react-express-tutorial?retryWrites=true&w=majority';
+const url = process.env.MONGODB_URI || `mongodb://localhost:27017`;
 const DB_NAME = 'react-express-tutorial';
 let db = null;
 
